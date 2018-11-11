dacad98f1bb6e2078531023f5e671d811e6f4bbc
Brandon Lyons <lyonsbp@umich.edu>
2018-11-11 13:30:46 -0500

    Updated index, deleted prompts

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 module.exports = (api, projectOptions) => { 
-  api.chainWebpack(webpackConfig => {
-    // modify webpack config with webpack-chain
-  })
 
-  api.configureWebpack(webpackConfig => {
-    // modify webpack config
-    // or return object to be merged with webpack-merge
-  })
-
-  api.registerCommand('test', args => {
-    // register `vue-cli-service test`
-  })
 }
